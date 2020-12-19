4e3bfd34831afe34d81d1153590e45b608a64a33
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-12-19 12:11:58 +0100

    started database model. Created User and Product model

---
+++ models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const ProductSchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Float64Array,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Product = mongoose.model("product", Product);
